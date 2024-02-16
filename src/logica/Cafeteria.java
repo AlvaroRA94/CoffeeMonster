e57c2c6878104eeba621d24349e2b7781702e729
Álvaro Roldán <roldan.antequera@gmail.com>
2024-02-16 11:52:59 +0100

    first commit

---
+++ src/logica/Cafeteria.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logica;
+
+import igu.Login;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Cafeteria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Login inicio = new Login();
+        inicio.setVisible(true);
+        inicio.setLocationRelativeTo(null);
+    }
+    
+}
